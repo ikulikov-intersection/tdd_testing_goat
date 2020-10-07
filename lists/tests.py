5bf5257a8705cce08705278dad80907ce0212a4a
Ivan Kulikov <ivan.kulikov@aeroespacial.sener>
2020-10-07 15:33:03 +0200

    first unit test and url mapping, with view

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -9,6 +10,12 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
 
+    def test_home_page_returns_correct_html(self):
+        request=HttpRequest()
+        response=home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 """
 class SmokeTest(TestCase):
     def test_bad_math(self):
